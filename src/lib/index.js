cc3a1cafac25dfb70e74355afad55e65a76d0b62
Joao Kiipper <jk@microcodigo.com>
2023-03-25 22:16:50 -0300

    expoted/exposed storeApi helper functions

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,3 +3,4 @@ import Svelvet from './container/views/Svelvet.svelte';
 export default Svelvet;
 // types/types.ts contains types that are exported to the sure
 export * from './types/types';
+export * from './store/controllers/storeApi.js';
